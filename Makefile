d5def2f5a9766c044f5425c88bc59e40288babc6
Yuki Abe <12268087+yukia3e@users.noreply.github.com>
2024-02-23 11:51:10 +0900

    Add case, comments

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ test:
 	docker compose up -d
 	echo "Waiting for database to start"
 	sleep 15
+	echo "======= Test without lenient ======="
 	-go run internal/main.go
+	echo "======= Test with lenient ======="
+	-go run internal/main.go -l true
 	docker compose down
--- internal/main.go
+++ internal/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
 
 	"cloud.google.com/go/spanner"
 	"github.com/rs/zerolog/log"
@@ -15,10 +16,17 @@ type Singer struct {
 	Name string `spanner:"Name"`
 }
 
+type SingerLowCase struct {
+	ID   int64  `spanner:"artistid"`
+	Name string `spanner:"name"`
+}
+
 func main() {
 	ctx := context.Background()
 
-	// Create a test database
+	withLenient := flag.Bool("l", false, "Use lenient mode")
+	flag.Parse()
+
 	client, deferFn := util.CreateTestDatabase(
 		ctx,
 		[]string{
@@ -30,7 +38,6 @@ func main() {
 	)
 	defer deferFn()
 
-	// Insert test data
 	if _, err := client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
 		_, err := txn.Update(ctx, spanner.Statement{
 			SQL: "INSERT INTO Singers (ArtistId, Name) VALUES (1, 'Alice'), (2, 'Bob'), (3, 'Charlie');",
@@ -40,7 +47,8 @@ func main() {
 		log.Fatal().Err(err).Msg("Failed to insert data")
 	}
 
-	// row.ToStruct => Success!
+	// Case 1: row.ToStruct => Success!
+	log.Info().Msg("start row.ToStruct")
 	func() {
 		rows := client.Single().Read(ctx, "Singers", spanner.Key{1}, []string{"ArtistId", "Name"})
 		defer rows.Stop()
@@ -63,18 +71,70 @@ func main() {
 		}
 		log.Info().Interface("singers", singers).Msg("row.ToStruct => Success!")
 	}()
+	log.Info().Msg("end row.ToStruct")
 
-	// spanner.SelectAll => Failed!
+	// Case 2: lower-case spanner.SelectAll WithLenient => Success!
+	log.Info().Msg("start lower-case spanner.SelectAll WithLenient")
 	func() {
 		iter := client.Single().Read(ctx, "Singers", spanner.AllKeys(), []string{"ArtistId", "Name"})
 		defer iter.Stop()
 
-		var singers []*Singer
+		var singers []*SingerLowCase
 		if err := spanner.SelectAll(iter, &singers, spanner.WithLenient()); err != nil {
 			// The error will not be output because it will panic in SelectAll
 			log.Fatal().Err(err).Msg("Failed to select all")
 		}
-		// Expected result, but the following line will not be output because it will panic in SelectAll
-		log.Info().Interface("singers", singers).Msg("spanner.SelectAll => Success!")
+		log.Info().Interface("singers", singers).Msg("lower-case spanner.SelectAll WithLenient => Success!")
 	}()
+	log.Info().Msg("end lower-case spanner.SelectAll WithLenient")
+
+	// Case 3: lower-case spanner.SelectAll => Success!
+	log.Info().Msg("start lower-case spanner.SelectAll")
+	func() {
+		iter := client.Single().Read(ctx, "Singers", spanner.AllKeys(), []string{"ArtistId", "Name"})
+		defer iter.Stop()
+
+		var singers []*SingerLowCase
+		if err := spanner.SelectAll(iter, &singers); err != nil {
+			// The error will not be output because it will panic in SelectAll
+			log.Fatal().Err(err).Msg("Failed to select all")
+		}
+		log.Info().Interface("singers", singers).Msg("lower-case spanner.SelectAll => Success!")
+	}()
+	log.Info().Msg("end lower-case spanner.SelectAll")
+
+	// memo: Switch between the case with and without WithLenient flag by flag to end with panic
+	if withLenient != nil && *withLenient {
+		// Case 4: spanner.SelectAll WithLenient => Failed!
+		log.Info().Msg("start spanner.SelectAll WithLenient")
+		func() {
+			iter := client.Single().Read(ctx, "Singers", spanner.AllKeys(), []string{"ArtistId", "Name"})
+			defer iter.Stop()
+
+			var singers []*Singer
+			if err := spanner.SelectAll(iter, &singers, spanner.WithLenient()); err != nil {
+				// The error will not be output because it will panic in SelectAll
+				log.Fatal().Err(err).Msg("Failed to select all")
+			}
+			// Expected result, but the following line will not be output because it will panic in SelectAll
+			log.Info().Interface("singers", singers).Msg("spanner.SelectAll WithLenient => Success!")
+		}()
+		log.Info().Msg("end spanner.SelectAll WithLenient")
+	} else {
+		// Case 5: spanner.SelectAll => Failed!
+		log.Info().Msg("start spanner.SelectAll")
+		func() {
+			iter := client.Single().Read(ctx, "Singers", spanner.AllKeys(), []string{"ArtistId", "Name"})
+			defer iter.Stop()
+
+			var singers []*Singer
+			if err := spanner.SelectAll(iter, &singers); err != nil {
+				// The error will not be output because it will panic in SelectAll
+				log.Fatal().Err(err).Msg("Failed to select all")
+			}
+			// Expected result, but the following line will not be output because it will panic in SelectAll
+			log.Info().Interface("singers", singers).Msg("spanner.SelectAll => Success!")
+		}()
+		log.Info().Msg("end spanner.SelectAll")
+	}
 }
